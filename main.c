1131f7e12604e25635d0fb57339ed175788f565b
LeTismey <124163690+NCHM62@users.noreply.github.com>
2023-05-08 19:28:25 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,98 +1,84 @@
-////////////////////////// TP10
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "prod.h"
 
-int lireProchaineCommande() //pour lire l'int contenu dans nextFact
-{
-FILE *f;
-int N;
-f=fopen("nextFact","r");
-fread(&N,sizeof(int),1,f);
-fclose(f);
-//printf("\n--->lu N=%d",N);
-return N;
-}
+int main(){
+    FILE *fp_commande, *fp_produit, *fp_stock, *fp_resultats;
+    int reference, quantite;
+    int reference2;
+    int reference3, quantite_totale;
+    int quantite_restante;
+    char nom_client[50], produit[50];
+    float prix_achat = 0, prix_unitaire;
+    int n = 0;
+    
+   
+    fp_produit = fopen("produits.txt", "rt");
+    fp_stock = fopen("stock.txt", "rt");
+    fp_resultats = fopen("nextFact", "w");
+    
+    for(int i = 0001; i <= 0003; i++){
+    char nom_fichier[30];
+    sprintf(nom_fichier, "commandes/commande%04d.txt", i);
+    fp_commande = fopen(nom_fichier, "r");
+    if(fp_commande == NULL){
+        printf("Erreur d'ouverture du fichier %s\n", nom_fichier);
+        return 1;
+    }
+    
+    if(fp_produit == NULL){
+        printf("Erreur d'ouverture du fichier produits.txt\n");
+        return 1;
+    }
+    
+    if(fp_stock == NULL){
+        printf("Erreur d'ouverture du fichier produits.txt\n");
+        return 1;
+    }
 
-void convertirNenChaine4(int N,char *N4) //convertit l'int N en une chaine de 4 caracteres
-{ // si N=1234 alors N4 sera égal à "1234"
-int m,c,d,u;
-char cm,cc,cd,cu;
-m=N/1000;cm=48+m;
-N%=1000;
-c=N/100;cc=48+c;
-N%=100;
-d=N/10;cd=48+d;
-u=N%10;
-cu=48+u;
-N4[0]=cm;N4[1]=cc;N4[2]=cd;N4[3]=cu;N4[4]='\0';
-}
-
-
-void lireLesCommandes() //cette fonction ouvre tous les fichiers commandeXXX.txt avec XXXX démarrant à N
-{
-FILE *ficCommande=NULL;
-int FINI=0;
-int N = lireProchaineCommande(); //numero de la premiere commande qui sera lue et traitee
-char NNNN[5];
-char nomCommande[29];
-
-do //ce do while prend fin dès que fichier commandeXXXX.txt est absent 
-	{
-	strcpy(nomCommande,"./commandes/commande");
-	convertirNenChaine4(N,NNNN); 
-	//printf("\n==>%s<==",NNNN);
-	ficCommande=NULL;
-	strcat(nomCommande,NNNN);
-	strcat(nomCommande,".txt");
+	if(fp_resultats == NULL){
+    printf("Erreur d'ouverture du fichier resultats.txt\n");
+    return 1;
+	}
 	
-	//printf("\n traitement de  %s",nomCommande);
+    // lire le nom du client
+    fscanf(fp_commande, "%s", nom_client);
 	
-	ficCommande=fopen(nomCommande,"rt");
-	if (ficCommande!=NULL)
-		{ // le fichier commandeNNNN.txt existe
-			printf("\n fichier %s present",nomCommande);
-			//lireCommande(nomCommande); // à vous de coder cette fonction lors de ce TP9
-			fclose(ficCommande);
+	fprintf(fp_resultats, "Résultats pour le client %s :\n\n", nom_client);
+    // boucle principale pour parcourir le fichier commande0001.txt
+    while(fscanf(fp_commande, "%d %d", &reference, &quantite) == 2){
+        // réinitialiser le pointeur de fichier fp_produit pour le lire à nouveau
+        rewind(fp_produit);
+        rewind(fp_stock);
+
+        // boucle pour chercher la référence dans le fichier produits.txt
+        while(fscanf(fp_produit, "%d %s %f", &reference2, produit, &prix_unitaire) == 3){
+            if(reference == reference2){
+                prix_achat = quantite * prix_unitaire;
+                fprintf(fp_resultats, "Le client %s doit payer %.2f euros pour ce produit %s\n", nom_client, prix_achat, produit);
+                break;
+            }
+        }
+        
+        while(fscanf(fp_stock, "%d %d", &reference3, &quantite_totale) == 2){
+            if(reference == reference3){
+                quantite_restante = quantite_totale - quantite;
+                fprintf(fp_resultats, "Il reste %d exemplaire(s) de l'article numéro %d\n\n", quantite_restante, n);
+                if(quantite_restante <= 0){
+					fprintf(fp_resultats, "ATTENTION !!! Nous n'avons plus l'article numéro %d dans le stock\n\n", n);
+					}
+                break;
+				}
+			}
+        fprintf(fp_resultats, "\n");
 		}
-	else
-		{
-			printf("\n toutes les commandes presentes ont ete traitees.");
-			FILE *f=fopen("nextFact","w"); // on va ecrire la valeur de N dans enxtFact 
-			// pour 
-			fwrite(&N,1,sizeof(int),f);
-			fclose(f);
-			FINI=1;			
-		}
+	}
 
-	N++;
-	}while(FINI==0);		
-
+    
+    fclose(fp_commande);
+    fclose(fp_produit);
+    fclose(fp_resultats);
+    
+    return 0;
 }
 
-
-
-int main()
-{
-	//creation d un fichier d'un seul int nommé nextFact et contenant l'int 1
-	// code à utiliser pour réinitialiser nextFact à 1 si besoin au cours du TP 
-	
-	/*
-	FILE *f;int N=1;
-	f=fopen("nextFact","w");
-	fwrite(&N,1,sizeof(int),f);
-	fclose(f);
-	*/ 	
-
-	//PARTIE 1 du TP : sans Gestion de stock
-	lireLesCommandes(); //lecture de tous les fichiers commandeXXX.txt (fichiers non traités jusqu'ici)	
-	
-
-	//PARTIE 2 du TP : avec Gestion de stock
-	//copiez coller votre travail précédent puis modifiez le  
-	//lireLesCommandes2(); 	
-
-	return 0;
-}
